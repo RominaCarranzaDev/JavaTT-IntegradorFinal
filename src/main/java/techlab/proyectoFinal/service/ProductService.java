205f6e3d435beaf0db825e865931d45bd7bbc666
Romina Carranza <romii.carranza@gmail.com>
2025-07-03 20:39:46 -0300

    add service for product

---
+++ src/main/java/techlab/proyectoFinal/service/ProductService.java
@@ -0,0 +1,34 @@
+package techlab.proyectoFinal.service;
+
+import techlab.proyectoFinal.dto.ProductDTO;
+import techlab.proyectoFinal.entity.Product;
+import techlab.proyectoFinal.repository.ProductRepository;
+
+import java.util.List;
+
+public class ProductService {
+    private ProductRepository repositoryJpa;
+
+    public ProductService( ProductRepository repositoryJpa) {
+        this.repositoryJpa = repositoryJpa;
+    }
+
+    public List<Product> listProducts() {
+//    return this.repository.listarProductos();
+        return this.repositoryJpa.findAll();
+    }
+
+
+
+    public ProductDTO addProduct(Product product) {
+
+        Product savedProduct = this.repositoryJpa.save(product);
+
+        ProductDTO responseDTO = new ProductDTO();
+        responseDTO.setMessage("Producto creado exitosamente");
+
+        return responseDTO;
+    }
+
+
+}
