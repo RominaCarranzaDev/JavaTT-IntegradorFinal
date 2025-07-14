0ce2cdc8f496752af756b42b4737854e1b073513
Romina Carranza <romii.carranza@gmail.com>
2025-07-14 15:28:29 -0300

    Add table Orden

---
+++ src/main/java/techlab/proyectoFinal/entity/Orden.java
@@ -0,0 +1,46 @@
+package techlab.proyectoFinal.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Setter
+@Getter
+@Entity
+public class Orden {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer cantidad;
+
+    @ManyToOne
+    @JoinColumn(name = "pedido")
+    private Pedido pedido;
+
+    @ManyToOne
+    @JoinColumn(name = "producto")
+    private Product producto;
+
+    private Double precioUn;
+
+    private Double subtotal;
+
+    public Orden() {}
+
+    public Orden(Integer cantidad, Pedido pedido, Product producto, Double precioUn) {
+        this.cantidad = cantidad;
+        this.pedido = pedido;
+        this.producto = producto;
+        this.precioUn = precioUn;
+    }
+
+    @PrePersist
+    @PreUpdate
+    public void calcularSubtotal() {
+        if (precioUn != null && cantidad != null) {
+            this.subtotal = precioUn * cantidad;
+        }
+    }
+}
--- src/main/java/techlab/proyectoFinal/entity/Pedido.java
+++ src/main/java/techlab/proyectoFinal/entity/Pedido.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
 
 @Setter
 @Getter
@@ -15,8 +16,32 @@ public class Pedido {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private LocalDateTime fecha;
-
     @OneToMany(mappedBy = "pedido", cascade = CascadeType.ALL)
     private List<Orden> ordenes = new ArrayList<>();
+
+    private LocalDateTime fecha = LocalDateTime.now();
+    private String cliente;
+    private Double total;
+    private String estado;
+    private String formaPago;
+
+    public Pedido(){}
+
+    public Pedido(String cliente, String estado, String formaPago){
+        this.cliente = cliente;
+        this.estado = estado;
+        this.formaPago = formaPago;
+    }
+
+    @PrePersist
+    @PreUpdate
+    public void calcularTotal() {
+        if (ordenes != null && !ordenes.isEmpty()) {
+            this.total = ordenes.stream()
+                    .mapToDouble(o -> o.getSubtotal() != null ? o.getSubtotal() : 0)
+                    .sum();
+        } else {
+            this.total = 0.0;
+        }
+    }
 }
