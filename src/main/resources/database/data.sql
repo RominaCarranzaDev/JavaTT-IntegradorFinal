016b26833151e648938181ddd2aadc0c2871c78b
Romina Carranza <romii.carranza@gmail.com>
2025-07-14 00:58:40 -0300

    add database H2

---
+++ src/main/resources/database/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO producto(`name`, price, stock, `description`, `category`, `image`, `name_normalized`)
+VALUES ('Café Cappuccino', 100, 1200, 'desc 1', 'cat1', 'img1', 'cafe cappuccino'),
+       ('Café Latte', 120, 1500, 'desc 2', 'cat2','img2','cafe latte'),
+       ('Mocha Frappuccino', 200, 1800, 'desc 3', 'cat3', 'img3', 'mocha frappuccino');
